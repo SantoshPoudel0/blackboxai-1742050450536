f334ce8579967532e95b9186abb34ad42ceaa1e2
Santosh Poudel <128451797+SantoshPoudel0@users.noreply.github.com>
2025-03-15 20:40:25 +0545

    Add client-setup.sh 1742050524916

---
+++ client-setup.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+cd client
+
+# Install dependencies
+npm install @mui/material @emotion/react @emotion/styled    # Material UI
+npm install @mui/icons-material                            # Material UI Icons
+npm install axios                                          # HTTP client
+npm install react-router-dom                               # Routing
+npm install @reduxjs/toolkit react-redux                   # State management
+npm install formik yup                                     # Form handling and validation
+npm install react-toastify                                 # Notifications
